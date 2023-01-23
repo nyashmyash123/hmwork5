9784679130005cebdfbeba34ff56b32245afd589
Nikita <makhonin_n_s@mail.ru>
2023-01-23 22:39:17 +0300

    enetial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        System.out.println("задание 1");
+        for (int i = 0; i <= 10; i++)
+            System.out.println( i );
+        //task 2
+        System.out.println("задание 2");
+        for (int i = 10; i >= 1; i = i - 1)
+            System.out.println( i );
+        // task 3
+        System.out.println("задание 3");
+        for (int i = 0; i < 17; i = i +2)
+            System.out.println( i );
+        // task 4
+         for (int i = 10; i >= -10; i--)
+             System.out.println( i );
+
+
     }
 }
+
